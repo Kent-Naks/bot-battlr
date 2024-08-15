f0c34cd8c9e11c233ee8882c21074fc0695dae3a
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-08-15 21:31:36 +0300

    available bots

--- bot-battlr/src/components/BotCollection.js
+++ bot-battlr/src/components/BotCollection.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import BotCard from './BotCard';
+
+// Component to display a collection of all available bots
+function BotCollection({ bots, addBotToArmy }) {
+    return (
+      <div className="bot-collection">
+        {/* Map over the bots array and render a BotCard for each bot */}
+        {bots.map(bot => (
+          <BotCard 
+            key={bot.id} 
+            bot={bot} 
+            handleClick={() => addBotToArmy(bot)} 
+          />
+        ))}
+      </div>
+    );
+  }
+  
+  export default BotCollection;
