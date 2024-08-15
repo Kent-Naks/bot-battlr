5090cbadc3da7efd380e896d47cd824a285d7212
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-08-15 20:51:58 +0300

    Imports to App.js

--- bot-battlr/src/App.js
+++ bot-battlr/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import BotCollection from "./components/BotCollection";
+import YourBotArmy from "./components/YourBotArmy";
 
 function App() {
   return (
