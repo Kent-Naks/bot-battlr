59e0fd371235184d9451c4f4b5ca3a0d723d8f39
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-08-15 21:08:49 +0300

    fetching bots data from server andfunction inputs

--- bot-battlr/src/App.js
+++ bot-battlr/src/App.js
@@ -3,22 +3,37 @@ import BotCollection from "./components/BotCollection";
 import YourBotArmy from "./components/YourBotArmy";
 
 function App() {
+  const [bots, setBots] = useState([]);
+  const [army, setArmy] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/bots")
+      .then((response) => response.json())
+      .then((data) => setBots(data));
+  }, []);
+
+  const addToArmy = (bot) => {
+    if (!army.find((b) => b.id === bot.id)) {
+      setArmy([...army, bot]);
+    }
+  };
+
+  const removeFromArmy = (bot) => {
+    setArmy(army.filter((b) => b.id !== bot.id));
+  };
+
+  const dischargeBot = (id) => {
+    fetch(`http://localhost:8001/bots/${id}`, { method: "DELETE" })
+      .then(() => {
+        setArmy(army.filter((bot) => bot.id !== id));
+        setBots(bots.filter((bot) => bot.id !== id));
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <YourBotArmy army={army} removeFromArmy={removeFromArmy} dischargeBot={dischargeBot} />
+      <BotCollection bots={bots} addToArmy={addToArmy} />
     </div>
   );
 }
